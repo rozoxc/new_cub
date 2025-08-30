37b17cec8d9cbc11a4716204b00303a715c0c905
Samir Elboukaoui <156188568+Selboukaoui@users.noreply.github.com>
2025-08-30 08:55:17 +0100

    Fix segf

--- parsing/parse.c
+++ parsing/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: selbouka <selbouka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 01:21:15 by selbouka          #+#    #+#             */
-/*   Updated: 2025/08/24 09:44:26 by ababdoul         ###   ########.fr       */
+/*   Updated: 2025/08/30 08:54:24 by selbouka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,34 @@
 
 
 int parser(char *file, t_game *game)
-    {
-     
-
-        game->player = ft_malloc(sizeof(t_player), 1);
-        if (!game->player)
-            return (err("Allocation failed\n"), 0);
+{
+    if (!file)
+        return (write(2, "File path is empty ! [./cub3d FILE_PATH]", 41), 0);
+    game->player = ft_malloc(sizeof(t_player), 1);
+    if (!game->player)
+        return (err("Allocation failed\n"), 0);
     game->player->dir_x = 0;
     game->player->dir_y = -1;
 
-        game->vars = ft_malloc(sizeof(t_vars), 1);
+    game->vars = ft_malloc(sizeof(t_vars), 1);
 
+    init_vars(game->vars);
+    if (!validate_path_and_open(file, game->vars))
+        return (0);
 
-        init_vars(game->vars);
-        if (!validate_path_and_open(file, game->vars))
-            return (0);
-
-        if (!parse_header(game->vars))
-        {
-            close(game->vars->fd);
-            return (0);
-        }
-
-        if (!parse_map(game))
-        {
-             close(game->vars->fd);
-             return (0);
-        }
-
+    if (!parse_header(game->vars))
+    {
         close(game->vars->fd);
-        printf("Parsing successful!\n"); 
-        return (1);
+        return (0);
     }
 
-// int main (int c , char **v)
-// {
-//     // printf ("%s\n", v[1]);
-//     parser(v[1]);
-// }
+    if (!parse_map(game))
+    {
+            close(game->vars->fd);
+            return (0);
+    }
+
+    close(game->vars->fd);
+    printf("Parsing successful!\n"); 
+    return (1);
+}
