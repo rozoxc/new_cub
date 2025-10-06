ad6bd4a005e65767fd5f29bce5964fe5fd5d98d8
Selboukaoui <samirelboukaoui12@gmail.com>
2025-10-06 21:52:09 +0100

    Bonus norms parse 100%%

--- bonus/parsing/doors_utils.c
+++ bonus/parsing/doors_utils.c
@@ -6,7 +6,7 @@
 /*   By: selbouka <selbouka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 19:34:44 by selbouka          #+#    #+#             */
-/*   Updated: 2025/10/06 19:38:55 by selbouka         ###   ########.fr       */
+/*   Updated: 2025/10/06 21:50:37 by selbouka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,20 +54,16 @@ int	check_door_in_ray(t_game *game, double *ray_x, double *ray_y,
 	return (0);
 }
 
-int	find_nearest_door(t_game *game, int *door_x, int *door_y)
+static int	raycast_find_door(t_game *game, double step_x, double step_y,
+			int *door_coords)
 {
 	double	ray_x;
 	double	ray_y;
-	double	step_x;
-	double	step_y;
-	int		door_coords[2];
 	int		i;
 	int		result;
 
 	ray_x = game->player->posX;
 	ray_y = game->player->posY;
-	step_x = game->player->dir_x * 0.1;
-	step_y = game->player->dir_y * 0.1;
 	i = 0;
 	while (i < 20)
 	{
@@ -75,14 +71,29 @@ int	find_nearest_door(t_game *game, int *door_x, int *door_y)
 		ray_y += step_y;
 		result = check_door_in_ray(game, &ray_x, &ray_y, door_coords);
 		if (result == 1)
-		{
-			*door_x = door_coords[0];
-			*door_y = door_coords[1];
 			return (1);
-		}
 		if (result == -1)
 			break ;
 		i++;
+	}
+	return (0);
+}
+
+int	find_nearest_door(t_game *game, int *door_x, int *door_y)
+{
+	double	step_x;
+	double	step_y;
+	int		door_coords[2];
+	int		res;
+
+	step_x = game->player->dir_x * 0.1;
+	step_y = game->player->dir_y * 0.1;
+	res = raycast_find_door(game, step_x, step_y, door_coords);
+	if (res == 1)
+	{
+		*door_x = door_coords[0];
+		*door_y = door_coords[1];
+		return (1);
 	}
 	return (0);
 }
